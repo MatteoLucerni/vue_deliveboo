f70aee0e60c7d7c9cfaf798a46e42812580da588
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-25 18:33:13 +0200

    feat: route to PlateDetailPage

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 // import pages
 import HomePage from '../pages/HomePage.vue';
+import PlateDetailPage from '../pages/PlateDetailPage.vue';
 
 // router
 const router = createRouter({
   history: createWebHistory(),
   linkExactActiveClass: 'active',
   // array of objects, every object is a route
-  routes: [{ path: '/', name: 'home', component: HomePage }],
+  routes: [
+    { path: '/', name: 'home', component: HomePage },
+    { path: '/plates/:id', name: 'plate-detail', component: PlateDetailPage },
+  ],
 });
 
 export { router };
