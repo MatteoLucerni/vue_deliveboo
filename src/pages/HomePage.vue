6a8e624d31725ad5cbe955bda3343a3ea39a643e
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-25 17:48:03 +0200

    feat: ajax call for plates and printed list

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from 'axios';
+export default {
+    components: {
+
+    },
+    data() {
+        return {
+            plates: [],
+            endpoint: 'http://127.0.0.1:8000/api/plates'
+        }
+    },
+    methods: {
+        fetchPlates() {
+            axios.get(this.endpoint).then(res => {
+                this.plates = res.data.plates
+                console.log('done')
+            }).catch(err => console.error(err)).then(res => {
+
+            })
+        }
+    },
+    created() {
+        this.fetchPlates()
+    }
+}
+</script>
+
+<template>
+    <h1>DeliveBoo</h1>
+    <ul>
+        <li v-for="plate in plates">
+            {{ plate.name }}
+        </li>
+    </ul>
+</template>
+
+<style scoped></style>
