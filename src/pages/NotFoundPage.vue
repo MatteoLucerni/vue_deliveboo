5e533e53b8015a243b9508879854760538ba7a93
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-25 18:38:22 +0200

    feat: added not found page with his routes

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'NotFoundPage'
+}
+</script>
+
+<template>
+    <div class="vh-100 d-flex flex-column justify-content-center align-items-center">
+        <h3>
+            404 | Page not found
+        </h3>
+        <button @click="$router.push({ name: 'home' })" class="btn btn-secondary mt-4">Go back home</button>
+    </div>
+</template>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 // import pages
 import HomePage from '../pages/HomePage.vue';
 import PlateDetailPage from '../pages/PlateDetailPage.vue';
+import NotFoundPage from '../pages/NotFoundPage.vue';
 
 // router
 const router = createRouter({
@@ -12,6 +13,8 @@ const router = createRouter({
   routes: [
     { path: '/', name: 'home', component: HomePage },
     { path: '/plates/:id', name: 'plate-detail', component: PlateDetailPage },
+    { path: '/not-found', name: 'not-found', component: NotFoundPage },
+    { path: '/:pathMatch(.*)*', redirect: '/not-found' },
   ],
 });
 
