10a9e9bc95e5a8561921e60970e25bf5bb0f6c92
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-27 11:22:30 +0200

    fix: create and edit connected to form

--- src/pages/CreatePlatePage.vue
+++ src/pages/CreatePlatePage.vue
@@ -1,4 +1,6 @@
 <script>
+import PlateForm from '../components/PlateForm.vue'; // Import the PlateForm component
+import axios from 'axios';
 
 const emptyPlate = {
   name: '',
@@ -6,70 +8,33 @@ const emptyPlate = {
   price: '',
   ingredients: '',
   description: '',
-}
+};
 
-import axios from 'axios';
 export default {
-  name: 'CreatePlatePage',
+  components: {
+    PlateForm, // Register the PlateForm component
+  },
   data() {
     return {
-      plate: {
-        name: '',
-        image: '',
-        price: '',
-        ingredients: '',
-        description: '',
-      }
+      plate: { ...emptyPlate },
     };
   },
   methods: {
-    createPlate() {
-
-      axios.post('http://127.0.0.1:8000/api/plates', this.plate).then(res => {
-        console.log('Success', res.data)
-        this.plate = emptyPlate
-        this.$router.push({ name: 'home' })
-      }).catch(err => console.error(err))
-
+    createPlate(plateData) {
+      axios.post('http://127.0.0.1:8000/api/plates', plateData)
+        .then((res) => {
+          console.log('Success', res.data);
+          this.plate = { ...emptyPlate }; // Reset the form
+          this.$router.push({ name: 'home' });
+        })
+        .catch((err) => console.error(err));
     },
   },
 };
 </script>
+
 <template>
-  <div class="d-flex align-items-center py-3">
-    <form @submit.prevent="createPlate">
-      <div class="mb-3">
-        <label for="plateName" class="form-label">Plate Name</label>
-        <input v-model.trim="plate.name" type="text" class="form-control" id="plateName" placeholder="Plate Name"
-          required>
-      </div>
-      <div class="mb-3">
-        <label for="plateImage" class="form-label">Plate Image</label>
-        <input v-model.trim="plate.image" type="url" class="form-control" id="plateImage" placeholder="Plate Image"
-          required>
-      </div>
-      <div class="mb-3">
-        <label for="platePrice" class="form-label">Plate Price</label>
-        <input v-model.trim="plate.price" type="text" class="form-control" id="platePrice" placeholder="Plate Price"
-          required>
-      </div>
-      <div class="mb-3">
-        <label for="plateIngredients" class="form-label">Plate Ingredients</label>
-        <textarea v-model.trim="plate.ingredients" class="form-control" id="plateIngredients"
-          placeholder="Plate Ingredients" required></textarea>
-      </div>
-      <div class="mb-3">
-        <label for="plateDescription" class="form-label">Plate Description</label>
-        <textarea v-model.trim="plate.description" class="form-control" id="plateDescription"
-          placeholder="Plate Description" required></textarea>
-      </div>
-
-      <!-- TODO: Add more details -->
-
-      <button type="submit" class="btn btn-primary">Create Plate</button>
-    </form>
+  <div>
+    <plate-form :plate="plate" mode="create" @create="createPlate" />
   </div>
-  <RouterLink class="btn btn-secondary" :to="{ name: 'home' }">
-    Go back to home
-  </RouterLink>
 </template>
--- src/pages/EditPlatePage.vue
+++ src/pages/EditPlatePage.vue
@@ -1,8 +1,11 @@
 <script>
-
+import PlateForm from '../components/PlateForm.vue'; // Import the PlateForm component
 import axios from 'axios';
+
 export default {
-  name: 'EditPlatePage',
+  components: {
+    PlateForm, // Register the PlateForm component
+  },
   data() {
     return {
       plate: {
@@ -11,64 +14,34 @@ export default {
         price: '',
         ingredients: '',
         description: '',
-      }
+      },
     };
   },
   methods: {
-    editPlate(id) {
-
-      axios.put('http://127.0.0.1:8000/api/plates/' + id, this.plate).then(res => {
-        console.log('Success', res.data)
-        this.$router.push({ name: 'home' })
-      }).catch(err => console.error(err))
-
+    editPlate(plateData) {
+      axios.put('http://127.0.0.1:8000/api/plates/' + this.$route.params.id, plateData)
+        .then((res) => {
+          console.log('Success', res.data);
+          this.$router.push({ name: 'home' });
+        })
+        .catch((err) => console.error(err));
     },
     getPlate() {
-      axios.get('http://127.0.0.1:8000/api/plates/' + this.$route.params.id).then(res => {
-        this.plate = res.data
-      }).catch(err => console.error(err))
-    }
+      axios.get('http://127.0.0.1:8000/api/plates/' + this.$route.params.id)
+        .then((res) => {
+          this.plate = res.data;
+        })
+        .catch((err) => console.error(err));
+    },
   },
   created() {
-    this.getPlate()
-  }
+    this.getPlate();
+  },
 };
 </script>
+
 <template>
-  <div class="d-flex align-items-center py-3">
-    <form @submit.prevent="editPlate(this.$route.params.id)">
-      <div class="mb-3">
-        <label for="plateName" class="form-label">Plate Name</label>
-        <input v-model.trim="plate.name" type="text" class="form-control" id="plateName" placeholder="Plate Name"
-          required>
-      </div>
-      <div class="mb-3">
-        <label for="plateImage" class="form-label">Plate Image</label>
-        <input v-model.trim="plate.image" type="url" class="form-control" id="plateImage" placeholder="Plate Image"
-          required>
-      </div>
-      <div class="mb-3">
-        <label for="platePrice" class="form-label">Plate Price</label>
-        <input v-model.trim="plate.price" type="text" class="form-control" id="platePrice" placeholder="Plate Price"
-          required>
-      </div>
-      <div class="mb-3">
-        <label for="plateIngredients" class="form-label">Plate Ingredients</label>
-        <textarea v-model.trim="plate.ingredients" class="form-control" id="plateIngredients"
-          placeholder="Plate Ingredients" required></textarea>
-      </div>
-      <div class="mb-3">
-        <label for="plateDescription" class="form-label">Plate Description</label>
-        <textarea v-model.trim="plate.description" class="form-control" id="plateDescription"
-          placeholder="Plate Description" required></textarea>
-      </div>
-
-      <!-- TODO: Add more details -->
-
-      <button type="submit" class="btn btn-primary">Edit Plate</button>
-    </form>
+  <div>
+    <plate-form :plate="plate" mode="edit" @edit="editPlate" />
   </div>
-  <RouterLink class="btn btn-secondary" :to="{ name: 'home' }">
-    Go back to home
-  </RouterLink>
 </template>
