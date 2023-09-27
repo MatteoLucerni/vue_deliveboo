5b6d77f887f790332cbb11e58e3971f4736982f8
Andrea Creazzi <andreacreazzi@libero.it>
2023-09-27 12:44:32 +0200

    feat AppAlert

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    props: { isOpen: Boolean, type: String, dismissable: Boolean },
+    emits: ['close']
+};
+</script>
+
+<template>
+    <div v-if="isOpen" :class="[`alert-${type || 'info'}`, { 'alert-dismissible': dismissable }]"
+        class="alert fade show my-3" role="alert">
+        <slot></slot>
+        <button type="button" class="btn close" @click="$emit('close')"></button>
+    </div>
+</template>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import { createApp } from 'vue';
-import './assets/scss/main.scss';
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
-import { router } from './router';
-import App from './App.vue';
+import { createApp } from "vue";
+import "./assets/scss/main.scss";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import { router } from "./router";
+import App from "./App.vue";
+import AppAlert from "./components/AppAlert.vue";
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).mount("#app");
