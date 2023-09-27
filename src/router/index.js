b64d95acbf86cc3cd4c87440c39244da06c6c010
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-27 15:06:13 +0200

    feat: route to trash page

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import PlateDetailPage from '../pages/PlateDetailPage.vue';
 import CreatePlatePage from '../pages/CreatePlatePage.vue';
 import EditPlatePage from '../pages/EditPlatePage.vue';
 import NotFoundPage from '../pages/NotFoundPage.vue';
+import TrashPage from '../pages/TrashPage.vue';
 
 // router
 const router = createRouter({
@@ -21,6 +22,7 @@ const router = createRouter({
       component: CreatePlatePage,
     },
     { path: '/plates/:id/edit', name: 'edit-plate', component: EditPlatePage },
+    { path: '/plates/trash', name: 'plates-trash', component: TrashPage },
     { path: '/not-found', name: 'not-found', component: NotFoundPage },
     { path: '/:pathMatch(.*)*', redirect: '/not-found' },
   ],
