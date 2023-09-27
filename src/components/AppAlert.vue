e28bc38be69228d46ba232fa85817a3718f2b494
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-27 18:59:29 +0200

    fix: fixed alert close button

--- src/components/AppAlert.vue
+++ src/components/AppAlert.vue
@@ -1,15 +1,15 @@
 <script>
 export default {
     props: { isOpen: Boolean, type: String, dismissable: Boolean },
-    emits: ['close']
+    emits: ['close'],
 };
 </script>
 
 <template>
     <div v-if="isOpen" :class="[`alert-${type || 'info'}`, { 'alert-dismissible': dismissable }]"
-        class="alert fade show my-3" role="alert">
+        class="alert fade show my-3 d-flex align-items-center justify-content-between " role="alert">
         <slot></slot>
-        <button type="button" class="btn close" @click="$emit('close')"></button>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" @click="$emit('close')"></button>
     </div>
 </template>
 
