bd2a91876c86fb74d93565762dc467d7cee01543
Andrea Creazzi <andreacreazzi@libero.it>
2023-09-27 12:51:06 +0200

    feat validation for edit

--- src/pages/EditPlatePage.vue
+++ src/pages/EditPlatePage.vue
@@ -1,10 +1,12 @@
 <script>
 import PlateForm from '../components/PlateForm.vue'; // Import the PlateForm component
+import AppAlert from '../components/AppAlert.vue';
 import axios from 'axios';
 
 export default {
   components: {
-    PlateForm, // Register the PlateForm component
+    PlateForm,
+    AppAlert // Register the PlateForm component
   },
   data() {
     return {
@@ -15,16 +17,41 @@ export default {
         ingredients: '',
         description: '',
       },
+      errors: {},
+      successMessage: null
     };
+  },
+  computed: {
+    hasErrors() {
+      return Object.keys(this.errors).length;
+    },
+    showAlert() {
+      return Boolean(this.hasErrors || this.successMessage);
+    },
+    alertType() {
+      return this.hasErrors ? 'danger' : 'success';
+    }
   },
   methods: {
     editPlate(plateData) {
+      this.errors = {};
+      this.successMessage = null;
       axios.put('http://127.0.0.1:8000/api/plates/' + this.$route.params.id, plateData)
         .then((res) => {
           console.log('Success', res.data);
-          this.$router.push({ name: 'home' });
+          this.successMessage = 'Piatto modificato con successo'
         })
-        .catch((err) => console.error(err));
+        .catch(err => {
+          if (err.response.status === 400) {
+            const { errors } = err.response.data;
+            const errorMessage = {};
+            for (let field in errors) errorMessage[field] = errors[field][0];
+            this.errors = errorMessage;
+          } else {
+            console.error(err);
+            this.errors = { network: 'Si è verificato un errore!' }
+          }
+        });
     },
     getPlate() {
       axios.get('http://127.0.0.1:8000/api/plates/' + this.$route.params.id)
@@ -42,6 +69,12 @@ export default {
 
 <template>
   <div>
+    <AppAlert :isOpen="showAlert" :type="alertType">
+      <div v-if="successMessage">{{ successMessage }}</div>
+      <ul v-if="hasErrors">
+        <li v-for="(error, field) in errors" :key="field">{{ error }}</li>
+      </ul>
+    </AppAlert>
     <plate-form :plate="plate" mode="edit" @edit="editPlate" />
   </div>
 </template>
