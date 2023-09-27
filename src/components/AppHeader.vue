33b2e32ce6132b34946027f359257344f5ab5da5
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-09-27 18:13:20 +0200

    feat: added subtitle

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,9 +2,10 @@
 </script>
 
 <template>
-    <header class="bg-light py-3 mb-5">
+    <header class="bg-light py-3">
         <div class="container">
             <h1 class="display-4">DeliveBoo</h1>
+            <i>"The place of the happy food"</i>
         </div>
     </header>
 </template>
