18ad29f55ce38c512d47df82f9bb8e3f63b4f886
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-10-03 12:26:51 +0200

    first draft bare bones nav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,20 +2,44 @@
 </script>
 
 <template>
-    <header class="bg-light py-3">
-        <div class="container d-flex align-items-center ">
-            <img class="logo" src="../../public/Yummy_Food.svg" alt="">
-            <div class="ms-3">
-                <h1 class="display-4">DeliveBoo</h1>
-                <i>"The happy food's place"</i>
+    <nav class="navbar navbar-expand-lg bg-body-db py-3 shadow-sm">
+        <div class="container-fluid d-flex align-items-center ">
+            <a class="navbar-brand d-flex align-items-center " href="#">
+                <img src="../../public/Yummy_Food.svg" alt="Logo" width="50" height="50"
+                    class="d-inline-block align-text-top rounded-5 mx-4">
+                DeliveBoo
+            </a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Disabled</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
         </div>
-    </header>
+    </nav>
 </template>
-
-<style scoped>
-.logo {
-    max-width: 110px;
-    border-radius: 50%;
-}
-</style>
