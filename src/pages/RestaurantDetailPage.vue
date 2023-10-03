e370e25ee8e5b76d1c53f5e4d17901a14b59ca60
Andrea Creazzi <andreacreazzi@libero.it>
2023-10-03 17:22:52 +0200

    feat: plates in RestaurantDetailPage

--- src/pages/RestaurantDetailPage.vue
+++ src/pages/RestaurantDetailPage.vue
@@ -11,20 +11,22 @@ export default {
     data() {
         return {
             endpoint: 'http://127.0.0.1:8000/api/restaurants/' + this.$route.params.id,
-            restaurant: {}
+            restaurant: {},
+            plates: {},
         }
     },
     methods: {
-        getrestaurant() {
+        getRestaurant() {
             axios.get(this.endpoint).then(res => {
-                this.restaurant = res.data
+                this.restaurant = res.data.restaurant,
+                    this.plates = res.data.plates
             }).catch(err => {
                 this.$router.push({ name: 'not-found' })
             })
         }
     },
     created() {
-        this.getrestaurant()
+        this.getRestaurant()
     }
 }
 </script>
@@ -35,7 +37,7 @@ export default {
         <AppHeader />
         <div class="container restaurant-card p-5 mt-5">
             <h1 class="mb-5 fw-bolder ">{{ restaurant.name }}</h1>
-            <div class="d-flex">
+            <div class="d-flex mb-3">
                 <img class="w-25 rounded-4 "
                     :src="restaurant.image ?? 'https://www.areafit.it/wp-content/uploads/2022/08/placeholder.png'"
                     :alt="restaurant.name"><br>
@@ -44,7 +46,9 @@ export default {
                     <small class="text-success fw-bold ">{{ restaurant.vat_number }}</small>
                 </div>
             </div>
-
+            <div v-for="plate in plates" :key="plate.id">
+                <p class="text-center">{{ plate.name }} {{ plate.category.name }} {{ plate.price }}</p>
+            </div>
             <button @click="$router.push({ name: 'home' })" class="button-main-db mt-4">Go back to home</button>
         </div>
     </AppLoader>
