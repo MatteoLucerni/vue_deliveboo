46f3724d80a499bfd46db950c9b50ccf13f2d4c6
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-10-04 16:34:53 +0200

    feat: added router link and removed usless links

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,4 +1,6 @@
 <script>
+import { RouterLink } from 'vue-router';
+
 </script>
 
 <template>
@@ -12,27 +14,13 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <RouterLink class="nav-link" :to="{ name: 'home' }">
+                            Home
+                        </RouterLink>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link">Disabled</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
+                    <li>
+                        <a class="nav-link" href="http://127.0.0.1:8000/">I am a restaurant owner <i
+                                class="redirect-icon fa-solid fa-arrow-up-right-from-square ms-1"></i></a>
                     </li>
                 </ul>
             </div>
