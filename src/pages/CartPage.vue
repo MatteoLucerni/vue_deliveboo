e042572a3c116af2ffb060cd07fe17f2659f358f
Andrea Creazzi <andreacreazzi@libero.it>
2023-10-05 12:23:02 +0200

    feat: CartPage added

---
+++ src/pages/CartPage.vue
@@ -0,0 +1,24 @@
+<script>
+import AppHeader from '../components/AppHeader.vue'
+export default {
+    components: {
+        AppHeader
+    }
+};
+</script>
+
+<template>
+    <AppHeader />
+    <div class="background-color-page py-3">
+        <div class="container">
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.background-color-page {
+    background-color: #ffebe3;
+    height: 765px;
+}
+</style>
