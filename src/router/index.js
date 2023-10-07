bf88a76c9188f131f35d5c5db4d308a765c07114
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-10-07 18:21:21 +0200

    TODO: removed eq for not found from router

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,34 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 
 // import pages
-import HomePage from "../pages/HomePage.vue";
-import RestaurantDetailPage from "../pages/RestaurantDetailPage.vue";
-import NotFoundPage from "../pages/NotFoundPage.vue";
-import CartPage from "../pages/CartPage.vue";
-import OrderFormPage from "../pages/OrderFormPage.vue";
-import OrderConfirmPage from "../pages/OrderConfirmPage.vue";
+import HomePage from '../pages/HomePage.vue';
+import RestaurantDetailPage from '../pages/RestaurantDetailPage.vue';
+import NotFoundPage from '../pages/NotFoundPage.vue';
+import CartPage from '../pages/CartPage.vue';
+import OrderFormPage from '../pages/OrderFormPage.vue';
+import OrderConfirmPage from '../pages/OrderConfirmPage.vue';
 
 // router
 const router = createRouter({
   history: createWebHistory(),
-  linkExactActiveClass: "active",
+  linkExactActiveClass: 'active',
   // array of objects, every object is a route
   routes: [
-    { path: "/", name: "home", component: HomePage },
+    { path: '/', name: 'home', component: HomePage },
     {
-      path: "/restaurants/:id",
-      name: "restaurant-detail",
+      path: '/restaurants/:id',
+      name: 'restaurant-detail',
       component: RestaurantDetailPage,
     },
-    { path: "/cart", name: "cart", component: CartPage },
-    { path: "/order-form", name: "order-form", component: OrderFormPage },
+    { path: '/cart', name: 'cart', component: CartPage },
+    { path: '/order-form', name: 'order-form', component: OrderFormPage },
     {
-      path: "/order-confirm",
-      name: "order-confirm",
+      path: '/order-confirm',
+      name: 'order-confirm',
       component: OrderConfirmPage,
     },
-    { path: "/not-found", name: "not-found", component: NotFoundPage },
-    { path: "/:pathMatch(.*)*", redirect: "/not-found" },
+    { path: '/not-found', name: 'not-found', component: NotFoundPage },
+    // { path: "/:pathMatch(.*)*", redirect: "/not-found" },
   ],
 });
 
