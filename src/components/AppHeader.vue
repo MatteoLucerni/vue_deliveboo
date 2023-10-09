822625bdbc1766645314fe61186d068666ffdfb6
Andrea Creazzi <andreacreazzi@libero.it>
2023-10-09 20:50:06 +0200

    fix: minor-fix AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -47,20 +47,28 @@ export default {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item" v-if="$route.name !== 'order-confirm'">
-                        <RouterLink class="nav-link" :to="{ name: 'home' }">
+                        <RouterLink class="nav-link home" :to="{ name: 'home' }">
                             <i class="fa-solid fa-house"></i>
                         </RouterLink>
                     </li>
                     <li>
-                        <a class="nav-link" href="http://127.0.0.1:8000/">I am a restaurant owner <i
+                        <a class="nav-link owner" href="http://127.0.0.1:8000/">I am a restaurant owner <i
                                 class="redirect-icon fa-solid fa-arrow-up-right-from-square ms-1"></i></a>
                     </li>
                 </ul>
             </div>
             <div class="d-flex justify-content-end align-item-center" v-if="$route.name !== 'order-confirm'">
-                <span class="text-dark me-2">{{ totalQuantity }}</span>
-                <RouterLink :to="{ name: 'cart' }"><i class="fa-solid fa-cart-shopping text-dark me-5 cart"></i>
+                <!-- <span class="text-dark me-2">{{ totalQuantity }}</span> -->
+                <RouterLink :to="{ name: 'cart' }">
+                    <button type="button" class="btn bg-cart position-relative">
+                        <i class="fa-solid fa-cart-shopping cart"></i>
+                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                            {{ totalQuantity }}
+                        </span>
+                    </button>
                 </RouterLink>
+                <!-- <RouterLink :to="{ name: 'cart' }"><i class="fa-solid fa-cart-shopping text-dark me-5 cart"></i>
+                </RouterLink> -->
             </div>
         </div>
     </nav>
