c7b0237dd9a12d0feb692cfb6c1f9575ea0b5596
Andrea Creazzi <andreacreazzi@libero.it>
2023-10-10 16:22:35 +0200

    fix: AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -46,7 +46,7 @@ export default {
             </a>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item" v-if="$route.name !== 'order-confirm'">
+                    <li class="nav-item" v-if="$route.name !== 'order-confirm' && $route.name !== 'home'">
                         <RouterLink class="nav-link home" :to="{ name: 'home' }">
                             <i class="fa-solid fa-house"></i>
                         </RouterLink>
@@ -58,7 +58,6 @@ export default {
                 </ul>
             </div>
             <div class="d-flex justify-content-end align-item-center" v-if="$route.name !== 'order-confirm'">
-                <!-- <span class="text-dark me-2">{{ totalQuantity }}</span> -->
                 <RouterLink :to="{ name: 'cart' }">
                     <button type="button" class="btn bg-cart position-relative">
                         <i class="fa-solid fa-cart-shopping cart"></i>
@@ -68,8 +67,6 @@ export default {
                         </span>
                     </button>
                 </RouterLink>
-                <!-- <RouterLink :to="{ name: 'cart' }"><i class="fa-solid fa-cart-shopping text-dark me-5 cart"></i>
-                </RouterLink> -->
             </div>
         </div>
     </nav>
